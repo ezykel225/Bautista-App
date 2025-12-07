bca0b7094ad6b79a9ecde22a006c1273c6cde5bb
unknown <COMPLAB2@LAB2-61.ASIANCOLLEGE.LOCAL>
2025-12-07 19:57:55 +0800

    Modified Text

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -18,7 +18,7 @@ export default function HomeScreen() {
         />
       }>
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Nasa Link Ang Comment Section, Thanks</ThemedText>
+        <ThemedText type="title">Nasa Link Ang Comment Section, Thanks Guys</ThemedText>
         <HelloWave />
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
